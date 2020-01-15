80f49529e0223dcd99797c824e4a426c23fbde75
isabroch <isabrochweb@gmail.com>
2020-01-15 16:19:31 +0100

    Change observe module to take query or DOMobject

--- src/scripts/observe.js
+++ src/scripts/observe.js
@@ -11,7 +11,7 @@
  * Use the {@link https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API Intersection Observer (IO) API}.<br>
  * Perform an action on a list of elements that match the selector, when they are within the threshold.
  *
- * @param {string} [selector] - The CSS selector for the elements that are targeted, as a string.
+ * @param {string | string[] | HTMLElement} [selector] - The target. If passed a string, use querySelector. If passed an array-wrapped string, use querySelectorAll. If passed a DOM Object, use that object.
  * @param {callback} [cb] - Action to perform on each element targeted by the selector.
  * @param {boolean} [firesOnce] - If true, will unobserve element after running the callback. False will not unobserve element.
  * @param {string} [rootMargin] - Root margin to be used with intersection observer API. Positive values are outset, negative values are inset. Read IO documentation for more information. All values must be px or %, even 0.
@@ -21,14 +21,16 @@
  */
 const observe = (
   selector = '.lazyLoad',
-  cb = function(el) {
+  cb = function (el) {
     console.log(`${el} has been spotted, and is doing nothing!`)
   },
   firesOnce = true,
   rootMargin = '0px 0px 50% 0px',
-  threshold = 0
+  threshold = 0,
+  rootOpt = null
 ) => {
   const options = {
+    root: rootOpt,
     rootMargin: rootMargin,
     threshold: threshold
   }
@@ -42,13 +44,22 @@ const observe = (
     });
   }
 
-  let listOfElements = document.querySelectorAll(selector)
+  let elements;
 
-  for (const element of listOfElements) {
-    new IntersectionObserver(callback, options).observe(element);
+  if (Array.isArray(selector)) {
+    elements = document.querySelectorAll(selector);
+    for (const element of elements) {
+      new IntersectionObserver(callback, options).observe(element);
+    }
+  } else if (typeof selector == "string") {
+    elements = document.querySelector(selector);
+    new IntersectionObserver(callback, options).observe(elements);
+  } else if (selector instanceof Element || selector instanceof HTMLDocument) {
+    elements = selector;
+    new IntersectionObserver(callback, options).observe(elements);
   }
 
-  return listOfElements;
+  return elements;
 };
 
 export default observe;
