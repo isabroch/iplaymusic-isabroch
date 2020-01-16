f738870ab7de22d71bb52b2f7bff57e3e2c4821b
isabroch <isabrochweb@gmail.com>
2020-01-16 17:25:18 +0100

    Add playlist scroll selection

---
+++ src/scripts/playlists.js
@@ -0,0 +1,10 @@
+const carousel = new Flickity( '.carousel__track', {
+  // options
+  cellAlign: 'center',
+  contain: true,
+  // percentPosition: false,
+  wrapAround: true,
+  imagesLoaded: true,
+  prevNextButtons: false,
+  pageDots: false,
+});
