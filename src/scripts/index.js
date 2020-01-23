ff33ef11e624ad3afa97b6c52fc49beffc124901
isabroch <isabrochweb@gmail.com>
2020-01-23 14:32:48 +0100

    Getting playlists

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 // Imports
 import observe from "./observe.js";
 import encodeParams from "./encodeParams.js";
+import randSample from "./randomFromArray.js";
 
 // FETCH
 async function auth() {
@@ -19,42 +20,59 @@ async function auth() {
 
   const response = await data.json();
 
-  console.log(response);
+  return `${response.token_type} ${response.access_token}`;
 }
 
-auth();
+( async function getData() {
+  const token = await auth();
 
-xfetch.init({
-  address: "https://reqres.in/api/",
-  key: ""
-});
-
-xfetch.get("users")
-  .then(result => {
-    const users = result.data;
-
-
-    for (const user of users) {
-      const cardContainer = document.querySelector('.featured-card-grid');
-      const cardTemplate = document.querySelector('#feature-card');
-      const cardClone = cardTemplate.content.cloneNode(true);
-
-      const cardFields = ['.feature-card__title', '.feature-card__type', '.feature-card__link', '.feature-card__background'].map(query => cardClone.querySelector(query));
-
-      cardFields[0].textContent = `${user.first_name} ${user.last_name}`;
-      cardFields[1].textContent = `${user.email}`;
-      cardFields[2].href = `#${user.id}`;
-      cardFields[3].dataset.image = `${user.avatar}`;
-
-      cardContainer.appendChild(cardClone);
-    }
-
-    observe(
-      ['.feature-card__background'],
-      function (el) {
-        el.style = `--card-bg: url(${el.dataset.image})`;
-      },
-      true,
-      '0px 0px 50% 0px'
-    );
+  xfetch.init({
+    address: "https://api.spotify.com/v1/",
+    key: token
   });
+
+  const playlists = (await xfetch.get(`search?q=%22music%22&type=playlist&limit=10`)).playlists.items;
+
+  console.log(playlists);
+
+  const featuredItems = []
+
+  for (const item of playlists) {
+    featuredItems.push(
+      {
+        name: item.name,
+        type: item.type,
+        id: item.id,
+        img: item.images[0].url
+      }
+    )
+  }
+
+  createCards(featuredItems);
+} )();
+
+function createCards(items) {
+  for (const item of items) {
+    const cardContainer = document.querySelector('.featured-card-grid');
+    const cardTemplate = document.querySelector('#feature-card');
+    const cardClone = cardTemplate.content.cloneNode(true);
+
+    const cardFields = ['.feature-card__title', '.feature-card__type', '.feature-card__link', '.feature-card__background'].map(query => cardClone.querySelector(query));
+
+    cardFields[0].textContent = item.name;
+    cardFields[1].textContent = item.type;
+    cardFields[2].href = `/playlists?id=${item.id}`;
+    cardFields[3].dataset.image = item.img;
+
+    cardContainer.appendChild(cardClone);
+  }
+
+  observe(
+    ['.feature-card__background'],
+    function (el) {
+      el.style = `--card-bg: url(${el.dataset.image})`;
+    },
+    true,
+    '0px 0px 50% 0px'
+  );
+}
--- src/scripts/playlists.js
+++ src/scripts/playlists.js
@@ -1,4 +1,7 @@
-const carousel = new Flickity( '.carousel__track', {
+import encodeParams from "./encodeParams.js";
+import randSample from "./randomFromArray.js";
+
+const carousel = new Flickity('.carousel__track', {
   // options
   cellAlign: 'center',
   contain: true,
@@ -8,3 +11,58 @@ const carousel = new Flickity( '.carousel__track', {
   prevNextButtons: false,
   pageDots: false,
 });
+
+async function auth() {
+  const paramString = encodeParams({
+    'grant_type': 'client_credentials'
+  });
+
+  const data = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: paramString,
+    headers: {
+      'Authorization': 'Basic MjYyNTNlOWY5NWQ5NDgzNzhlMWU3MGQ5NTUyYTZlZmE6NjQ4OGE5MzU3MWViNGRkODhjOTkzNDQ0YTdmNGM3NWM=',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  const response = await data.json();
+
+  return `${response.token_type} ${response.access_token}`;
+}
+
+(async function doThings() {
+  const token = await auth();
+
+  xfetch.init({
+    address: "https://api.spotify.com/v1/",
+    key: token
+  });
+
+  const playlistId = new URLSearchParams(window.location.search).get('id');
+
+  async function getPlaylistTracksById(id) {
+    const data = (await xfetch.get(`playlists/${id}/tracks`)).items;
+
+    console.log(data);
+
+    const tracks = data.map(entry => {
+      if (entry.track) {
+        return {
+          name: entry.track.name,
+          artist: entry.track.artists[0].name,
+          id: entry.track.id
+        }
+      }
+    });
+
+
+    for (const track of tracks) {
+      if (track) {
+        document.querySelector('.songs').innerHTML += `<li class="song"><a href="spotify:track:${track.id}">${track.name} by ${track.artist}</a></li>`;
+      }
+    }
+  }
+
+  getPlaylistTracksById(playlistId);
+})();
