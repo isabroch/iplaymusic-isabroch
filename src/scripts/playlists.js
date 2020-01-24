5ffffb0f4b28c0991e73a80ed68f4d74fca3e285
isabroch <isabrochweb@gmail.com>
2020-01-24 09:31:18 +0100

    Load playlists

--- src/scripts/playlists.js
+++ src/scripts/playlists.js
@@ -1,18 +1,7 @@
 import auth from "./authentication.js";
 
-const carousel = new Flickity('.carousel__track', {
-  // options
-  cellAlign: 'center',
-  contain: true,
-  wrapAround: true,
-  imagesLoaded: true,
-  prevNextButtons: false,
-  pageDots: false,
-  initialIndex: '.is-initial-select'
-});
 
-
-(async function doThings() {
+const getAllPlaylists = (async function() {
   const token = await auth();
 
   xfetch.init({
@@ -20,48 +9,61 @@ const carousel = new Flickity('.carousel__track', {
     key: token
   });
 
-  const playlistId = new URLSearchParams(window.location.search).get('id');
+  const createPlaylistObject = function(data, current = false) {
+    const playlists = {};
 
-  async function getPlaylistTracksById(id) {
-    const data = (await xfetch.get(`playlists/${id}/tracks`)).items;
-
-    console.log(data);
-
-    const tracks = data.map(entry => {
-      if (entry.track) {
-        return {
-          name: entry.track.name,
-          artist: entry.track.artists[0].name,
-          id: entry.track.id
-        }
-      }
-    });
-
-
-    for (const track of tracks) {
-      if (track) {
-        document.querySelector('.songs').innerHTML += `<li class="song"><a href="spotify:track:${track.id}">${track.name} by ${track.artist}</a></li>`;
-      }
+    const createPlaylistItem = (item) => {
+      playlists[item.id] = {name: item.name, img: item.images[0].url, current: current}
     }
+
+    for (const playlist of data) {
+      createPlaylistItem(playlist);
+    }
+
+    return playlists;
   }
 
-  getPlaylistTracksById(playlistId);
+  const getUserPlaylists = async function() {
+    const userPlaylists = (await xfetch.get(`me/playlists`)).items;
+
+    return createPlaylistObject(userPlaylists);
+  }
+
+  const getPagePlaylist = async function() {
+    const pagePlaylistId = new URLSearchParams(window.location.search).get('id');
+
+    if(pagePlaylistId) {
+      const pagePlaylist = (await xfetch.get(`playlists/${pagePlaylistId}`));
+
+      return createPlaylistObject([pagePlaylist], true);
+    }
+
+    return '';
+  }
+
+  const userPlaylists = await getUserPlaylists();
+  const pagePlaylist = await getPagePlaylist();
+
+  const playlists = {...userPlaylists, ...pagePlaylist};
+
+  return playlists;
 })();
+
 
 /* PSUEDO CODE:
 
-  GET DATA ON PAGE LOAD
-    create playlists{} that holds playlistDetails (thumbnail, name)
-
-    get a list of all user's playlists
-    ~ look at [me/playlists] @ https://developer.spotify.com/documentation/web-api/reference/playlists/get-a-list-of-current-users-playlists/
-
-    for each playlist, create playlists[id] with playlistDetails{}
-
-    if param ?id exists, get playlist
-    ~ look at [playlists/{id}] @ https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlist/
-
-    create playlists[?id] with playlistDetails{... current: true}
+//  GET DATA ON PAGE LOAD
+//    create playlists{} that holds playlistDetails (thumbnail, name)
+//
+//    get a list of all user's playlists
+//    ~ look at [me/playlists] @ https://developer.spotify.com/documentation/web-api/reference/playlists/get-a-list-of-current-users-playlists/
+//
+//    for each playlist, create playlists[id] with playlistDetails{}
+//
+//    if param ?id exists, get playlist
+//    ~ look at [playlists/{id}] @ https://developer.spotify.com/documentation/web-api/reference/playlists/get-playlist/
+//
+//    create playlists[?id] with playlistDetails{... current: true}
 
   CREATE PLAYLIST CAROUSEL
     for ${id} in playlists{}...
